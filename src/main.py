84f006e2688efe0c32c288555e8e20ef9244e2aa
Jenushan <jenushan349@gmail.com>
2026-04-26 15:55:06 -0400

    feat: add check for missing security headers in /scan endpoint

--- src/main.py
+++ src/main.py
@@ -15,6 +15,9 @@ class ScanRequest(BaseModel):
 @app.post("/scan") 
 def scan(data: ScanRequest):
   
+  security_header = ["Content-Security-Policy", "X-Frame-Options", "X-Content-Type-Options", "Strict-Transport-Security", "Referrer-Policy"]
+  findings = []
+
   if data.url.startswith("https://") or data.url.startswith("http://"):
     try: 
       response = requests.get(data.url, timeout = 5)
@@ -23,9 +26,15 @@ def scan(data: ScanRequest):
   else: 
     return {"error": "Invalid URL. URL must start with http:// or https://" }
 
+  for header in security_header:
+    if header not in response.headers: 
+      findings.append("Missing " + header)
+  
   return {
     "target_url": data.url,
     "status_code": response.status_code,
     "headers": dict(response.headers), 
-    "data": response.json()
+    "data": response.json(),
+    "findings": findings
   }
+
