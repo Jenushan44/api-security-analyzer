be5bfda1683f69a8002fedad21861580e27c4a5c
Jenushan <jenushan349@gmail.com>
2026-05-23 16:56:58 -0400

    style: add styling to risk level column

--- frontend/components/scan-history/ScanHistoryTable.tsx
+++ frontend/components/scan-history/ScanHistoryTable.tsx
@@ -2,8 +2,23 @@ import { ScanHistoryItem } from "../../types/scan";
 
 function ScanHistoryTable({ scans }: { scans: ScanHistoryItem[] }) {
 
+  function getSeverityCardStyle(severity: string) {
+
+    if (severity == "Critical") {
+      return "border border-red-500/70 bg-red-500/10 text-red-400 px-2 py-1 font-semibold"
+    } else if (severity == "High") {
+      return "border border-orange-500/70 bg-orange-500/10 text-orange-400 px-2 py-1 font-semibold"
+    } else if (severity == "Medium") {
+      return "border border-yellow-500/70 bg-yellow-500/10 text-yellow-400 px-2 py-1 font-semibold"
+    } else if (severity == "Low") {
+      return "border border-green-500/70 bg-green-500/10 text-green-400 px-2 py-1 font-semibold"
+    } else {
+      return "border border-gray-500/70 bg-gray-500/10 text-gray-400 px-2 py-1 font-semibold"
+    }
+  }
+
   return (
-    <div>
+    <div className="px-6 mt-5">
 
       <table className="w-full">
         <thead>
@@ -23,9 +38,11 @@ function ScanHistoryTable({ scans }: { scans: ScanHistoryItem[] }) {
             <tr key={scan.id} className="text-gray-300 w-full px-4 py-3 text-sm">
               <td className="border border-[#1E293B] text-center px-4 py-3">{scan.id}</td>
               <td className="border border-[#1E293B] text-center px-4 py-3">{scan.target_url}</td>
-              <td className="border border-[#1E293B] px-4 py-3">{scan.risk_score}</td>
-              <td className="border border-[#1E293B] px-4 py-3 text-blue-400">{scan.risk_level}</td>
-              <td className="border border-[#1E293B] px-4 py-3">{scan.total_findings}</td>
+              <td className="border border-[#1E293B] px-4 py-3 text-center">{scan.risk_score}</td>
+              <td className="border border-[#1E293B] px-4 py-3 text-blue-400 text-center">
+                <span className={getSeverityCardStyle(scan.risk_level)}>{scan.risk_level}</span>
+              </td>
+              <td className="border border-[#1E293B] px-4 py-3 text-center">{scan.total_findings}</td>
               <td className="border border-[#1E293B] text-center px-4">{scan.status_code}</td>
               <td className="border border-[#1E293B] px-4 py-2">{scan.created_at}</td>
             </tr>)
