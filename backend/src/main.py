76569c76530b378c371561ac5a6dbe21723ca094
Jenushan <jenushan349@gmail.com>
2026-06-29 20:43:10 -0400

    fix: allow all vercel origins

--- backend/src/main.py
+++ backend/src/main.py
@@ -19,17 +19,11 @@ import time
 app = FastAPI() 
 
 app.add_middleware(
-  CORSMiddleware, 
-  allow_origins = [  
-    "http://localhost:3000",
-    "http://localhost:3001",
-    "http://127.0.0.1:3000",
-    "http://127.0.0.1:3001",
-    "https://api-security-analyzer.vercel.app",
-    ], 
-  allow_credentials = True,   
-  allow_methods = ["*"], 
-  allow_headers = ["*"],
+    CORSMiddleware,
+    allow_origin_regex=r"https://.*\.vercel\.app",
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
 @app.get("/") 
